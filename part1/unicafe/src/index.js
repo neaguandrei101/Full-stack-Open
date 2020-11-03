671f89ebc15cc43dcad6725d574b5027e1339bf2
Andrei Neagu <neaguandrei47@gmail.com>
2020-11-03 14:37:40 +0100

    1.7 done

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -5,12 +5,19 @@ const Button = ({ handleClick, text }) =>
   <button onClick={handleClick}>
     {text}
   </button>
+const Statistics = (props) => {
+  // ...
+}
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good + neutral + bad
+  const average = (good + bad*-1) / all
+  const positive = good * 100 / all
+
 
   return (
     <div>
@@ -22,7 +29,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
-
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
