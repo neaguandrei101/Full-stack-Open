54265527b61fb147c5ba2ba5cd2a2d2010fd06b5
Andrei Neagu <neaguandrei47@gmail.com>
2020-11-04 22:11:12 +0100

    almost done

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,47 +1,65 @@
-import React, { useState } from 'react'
+import React, { Fragment, useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const Button = ({ handleClick, text }) =>
   <button onClick={handleClick}>
     {text}
   </button>
-const Statistics = ({good, neutral, bad, all, average, positive, setGood, setNeutral, setBad}) => 
-  <div>
+
+const Statistics = ({ good, neutral, bad, all, average, positive, setGood, setNeutral, setBad }) => {
+    if(all === 0)
+    return (
+      <div>
+        <h1>give feedback</h1>
+        <Statistic field={good} setter={setGood} text='good' />
+        <Statistic field={neutral} setter={setNeutral} text='neutral' />
+        <Statistic field={bad} setter={setBad} text='bad' />
+        <h1>statistics</h1>
+        <p>no feedback given</p>
+      </div>
+    )
+      return(
+    <div>
       <h1>give feedback</h1>
-      <Button handleClick={() => setGood(good + 1)} text='good' />
-      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
-      <Button handleClick={() => setBad(bad + 1)} text='bad' />
+      <Statistic field={good} setter={setGood} text='good' />
+      <Statistic field={neutral} setter={setNeutral} text='neutral' />
+      <Statistic field={bad} setter={setBad} text='bad' />
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
       <p>all {all}</p>
       <p>average {average}</p>
       <p>positive {positive} %</p>
     </div>
+    )
+  
+}
 
+
+
+const Statistic = ({ field, setter, text }) =>
+  <Fragment>
+    <Button handleClick={() =>{setter(field + 1)}} text={text} />
+    <p>{text} {field}</p>
+  </Fragment>
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const all = good + neutral + bad
-  const average = (good + bad*-1) / all
+  const average = (good + bad * -1) / all
   const positive = good * 100 / all
-
 
   return (
     <div>
-      <Statistics good = {good} 
-      neutral = {neutral}
-      bad = {bad}
-      all = {all}
-      average = {average}
-      positive = {positive}
-      setGood = {setGood}
-      setBad = {setBad}
-      setNeutral = {setNeutral} 
+      <Statistics good={good}
+        neutral={neutral}
+        bad={bad}
+        all={all}
+        average={average}
+        positive={positive}
+        setGood={setGood}
+        setBad={setBad}
+        setNeutral={setNeutral}
       />
     </div>
   )
