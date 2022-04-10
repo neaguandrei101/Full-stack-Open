bfdfdc6f498c7621ff486526588cbad55f165602
Andrei Neagu <neaguandrei47@gmail.com>
2022-04-10 16:37:49 +0300

    part 4 a done

--- part4/README.md
+++ part4/README.md
@@ -1,1 +1,7 @@
 # Exercises done
+## Part 4
+
+4.1 done
+4.2 done
+4.3 done
+4.4 done
---
+++ part4/backend/utils/list_helper.js
@@ -0,0 +1,18 @@
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  const likes = blogs.map(blog => blog.likes)
+
+  const reducer = (sum, item) => {
+    return sum + item
+  }
+
+  return likes.reduce(reducer, 0)
+}
+
+module.exports = {
+  dummy,
+  totalLikes
+}
