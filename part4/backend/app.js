a4822e99c3846095295cd3f285a0dfa7338eb08c
Andrei Neagu <neaguandrei47@gmail.com>
2022-06-17 21:04:22 +0300

    4.15 done

--- part4/backend/app.js
+++ part4/backend/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require("./controllers/users");
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -19,6 +20,7 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/users', usersRouter)
 app.use('/', blogsRouter)
 app.use(middleware.errorHandler)
 
