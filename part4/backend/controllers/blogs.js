2dae72a3bb0b68e14b832e322606136a3dc731fa
Andrei Neagu <neaguandrei47@gmail.com>
2022-06-23 18:46:28 +0300

    4.20 done
    error handling fixed

--- part4/backend/controllers/blogs.js
+++ part4/backend/controllers/blogs.js
@@ -1,52 +1,57 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
-blogRouter.get('/api/blogs', async (request, response) => {
-  const blogs = await Blog
-      .find({}).populate('user', {username: 1, name: 1})
-  response.json(blogs)
+blogRouter.get('/', async (request, response) => {
+    const blogs = await Blog
+        .find({}).populate('user', {username: 1, name: 1})
+    response.json(blogs)
 })
 
-blogRouter.post('/api/blogs', async (request, response) => {
-  const body = request.body
-  const user = await User.findOne()
+blogRouter.post('/', async (request, response) => {
+    const body = request.body
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({error: 'token missing or invalid'})
+    }
+    const user = await User.findById(decodedToken.id)
 
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    user: user._id,
-    likes: body.likes,
-  })
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        user: user._id,
+        likes: body.likes,
+    })
 
-  const savedBlog = await blog.save()
-  user.blogs = user.blogs.concat(savedBlog._id)
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
 
-  await user.save()
+    await user.save()
 
-  response.status(201).json(savedBlog)
+    response.status(201).json(savedBlog)
 
 })
 
-blogRouter.delete('/api/blogs/:id', async (request, response) => {
-  const id = request.params.id
+blogRouter.delete('/:id', async (request, response) => {
+    const id = request.params.id
 
-  const deleteMe = await Blog.deleteOne({ _id: id })
-  response.json(deleteMe)
+    const deleteMe = await Blog.deleteOne({_id: id})
+    response.json(deleteMe)
 })
 
-blogRouter.put('/api/blogs/:id', async (request, response) => {
-  const id = request.params.id
-  const blog = {
-    likes: request.body.likes,
-  }
+blogRouter.put('/:id', async (request, response) => {
+    const id = request.params.id
+    const blog = {
+        likes: request.body.likes,
+    }
 
-  const updatedBlog = await Blog.findByIdAndUpdate(id, blog)
+    const updatedBlog = await Blog.findByIdAndUpdate(id, blog)
 
-  updatedBlog
-    ? response.json(updatedBlog)
-    : response.status(404).end()
+    updatedBlog
+        ? response.json(updatedBlog)
+        : response.status(404).end()
 })
 
 module.exports = blogRouter
--- part4/backend/utils/middleware.js
+++ part4/backend/utils/middleware.js
@@ -1,13 +1,36 @@
+const logger = require('./logger')
+
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+    logger.error(error.name, error.message)
 
     if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
+        return response.status(400).send({error: 'malformatted id'})
     } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
+        return response.status(400).json({error: error.message})
+    } else if (error.name === 'JsonWebTokenError') {
+        return response.status(401).json({error: 'invalid token'})
     }
 
     next(error)
 }
 
-module.exports = { errorHandler }
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const tokenExtractor = (request, response, next) => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        request.token = authorization.substring(7)
+    } else {
+        request.token = null
+    }
+
+    next()
+}
+
+module.exports = {
+    errorHandler,
+    unknownEndpoint,
+    tokenExtractor
+}
