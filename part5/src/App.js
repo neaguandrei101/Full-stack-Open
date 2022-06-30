724e8e68ea5de80615ecdd896fe9bfedf139ce8e
Andrei Neagu <neaguandrei47@gmail.com>
2022-06-30 09:08:31 +0300

    5.4 done

--- part5/src/App.js
+++ part5/src/App.js
@@ -3,12 +3,17 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import React from 'react';
+import Notification from "./components/Notification";
 
 const App = () => {
     const [blogs, setBlogs] = useState([])
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const [user, setUser] = useState(null)
+    const [title, setTitle] = useState('')
+    const [author, setAuthor] = useState('')
+    const [url, setUrl] = useState('')
+    const [message, setMessage] = useState(null)
 
     useEffect(() => {
         blogService.getAll().then(blogs =>
@@ -69,13 +74,13 @@ const App = () => {
         </React.Fragment>
     )
 
-    const blogForm = () => (
+    const blogAll = () => (
         blogs.map(blog =>
             <Blog key={blog.id} blog={blog}/>
         )
     )
 
-    const loggedInForm = () => (
+    const loggedIn = () => (
         <div>
             <p>{user.name} logged-in</p>
             <button onClick={() => {
@@ -84,21 +89,78 @@ const App = () => {
             }}>
                 logout
             </button>
+            <form onSubmit={handleCreateNew}>
+                <div>
+                    title
+                    <input
+                        type="text"
+                        value={title}
+                        name="Title"
+                        onChange={({target}) => setTitle(target.value)}
+                    />
+                </div>
+                <div>
+                    author
+                    <input
+                        type="text"
+                        value={author}
+                        name="Author"
+                        onChange={({target}) => setAuthor(target.value)}
+                    />
+                </div>
+                <div>
+                    url
+                    <input
+                        type="text"
+                        value={url}
+                        name="Url"
+                        onChange={({target}) => setUrl(target.value)}
+                    />
+                </div>
+                <button type="submit">create</button>
+            </form>
         </div>
     )
 
+    const handleCreateNew = async (event) => {
+        event.preventDefault()
+        try {
+            const returnedBlog = await blogService.create({
+                title,
+                author,
+                url
+            })
+            setTitle('')
+            setAuthor('')
+            setUrl('')
+            setMessage('New blog added')
+            setTimeout(() => {
+                setMessage(null)
+            }, 5000)
+
+        } catch (e) {
+            console.error('cant create new blog', e.message)
+            setMessage('cant create new blog')
+            setTimeout(() => {
+                setMessage(null)
+            }, 5000)
+        }
+
+    }
+
     return (
         <div>
+            <Notification message={message}/>
             <h2>Login</h2>
 
             {user === null
                 ? loginForm()
-                : loggedInForm()
+                : loggedIn()
             }
 
             <h2>blogs</h2>
 
-            {blogForm()}
+            {blogAll()}
         </div>
     )
 }
