b69abe4717661bca0e3892cccacd789bd0582967
Andrei Neagu <neaguandrei47@gmail.com>
2022-07-24 22:08:09 +0300

    5.21 done

--- part5/cypress/e2e/spec.cy.js
+++ part5/cypress/e2e/spec.cy.js
@@ -4,6 +4,39 @@ describe('Blog app', function () {
         cy.visit('http://localhost:3000')
     })
 
+    describe('When logged in', function () {
+        beforeEach(() => {
+            cy.request('POST', 'http://localhost:3003/api/users', {
+                'username': 'root',
+                'name': 'Superuser',
+                'password': 'saline'
+            }).then(res => {
+                expect(res.status).to.equal(201)
+            })
+
+            cy.login({username: 'root', password: 'saline'})
+            cy.createBlog({title: 'cy test', author: 'bot', url: 'not in reality', likes: 0})
+            cy.createBlog({title: 'another blog', author: 'anotherUser', url: 'not in reality2', likes: 0})
+
+        })
+
+        it('A blog can be created', function () {
+            cy.contains('cy test')
+        })
+
+        it('users can like a blog',() => {
+            cy.contains('cy test').find('button').filter(':contains("like")').click()
+            cy.contains('1')
+        })
+
+        it('right user can delete a blog', () => {
+            cy.contains('cy test').find('button').filter(':contains("remove")').click()
+            cy.on('window:confirm', () => true)
+            cy.contains('cy test').should('not.exist')
+
+        })
+    })
+
     describe('Login', function () {
         beforeEach(function () {
             cy.request('POST', 'http://localhost:3003/api/users', {
@@ -31,32 +64,6 @@ describe('Blog app', function () {
             cy.get('#login-button').click()
 
             cy.contains('login')
-        })
-    })
-
-    describe('When logged in', function () {
-        beforeEach(() => {
-            cy.request('POST', 'http://localhost:3003/api/users', {
-                'username': 'root',
-                'name': 'Superuser',
-                'password': 'saline'
-            }).then(res => {
-                expect(res.status).to.equal(201)
-            })
-
-            cy.login({username: 'root', password: 'saline'})
-            cy.createBlog({title: 'cy test', author: 'bot', url: 'not in reality', likes: 0})
-            cy.createBlog({title: 'cy test2', author: 'bot2', url: 'not in reality2', likes: 0})
-
-        })
-
-        it('A blog can be created', function () {
-            cy.contains('cy test')
-        })
-
-        it('users can like a blog',() => {
-            cy.contains('cy test').find('button').filter(':contains("like")').click()
-            cy.contains('1')
         })
     })
 })
