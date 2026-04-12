13c421a9fdd683484d9e462a4971656503e1664d
Andrei Neagu <neaguandrei47@gmail.com>
2026-04-13 02:58:30 +0300

    9.17 done

--- part9/vite react/frontend flight diary/src/App.tsx
+++ part9/vite react/frontend flight diary/src/App.tsx
@@ -1,11 +1,46 @@
+import * as React from "react";
+import {useEffect} from "react";
+import axios from "axios";
+
+interface JournalLog {
+    "id": number;
+    "date": string;
+    "weather": string;
+    "visibility": string;
+}
+
+const JournalElement = ({journalLogs}: { journalLogs: JournalLog[] }): React.ReactElement => {
+    return (
+        <div>
+            {journalLogs.map((journal) => (
+                    <div key={journal.id}>
+                        <h3>Date: {journal.date}</h3>
+                        <p>Weather: {journal.weather}</p>
+                        <p>Visibility: {journal.visibility}</p>
+                    </div>)
+            )}
+        </div>
+    );
+};
+
+
 
 function App() {
 
-  return (
-    <div>
-        <p>Hello world</p>
-    </div>
-  )
+    const [journalLogs, setJournalLogs] = React.useState<JournalLog[]>([]);
+
+    useEffect(() => {
+        axios.get<JournalLog[]>('http://localhost:3000/api/diaries').then(response => {
+            setJournalLogs(response.data)
+        })
+    }, [])
+
+    return (
+        <div>
+            <h1>Diary entries</h1>
+            <JournalElement journalLogs={journalLogs} />
+        </div>
+    )
 }
 
 export default App
